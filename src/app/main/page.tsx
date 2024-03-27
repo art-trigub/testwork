94119bb91fa1060d4abeeaa62b335eb32baa9e33
Artem Tryhub <art.trigub@gmail.com>
2024-03-28 00:52:13 +0200

    ver1.0

---
+++ src/app/main/page.tsx
@@ -0,0 +1,10 @@
+function page() {
+  return (
+    <div>
+        <h1 className="text-blue">We make  creative media that <span className="text-darkblue">adds value</span></h1>
+    </div>
+    
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,160 @@
 import Image from "next/image";
+import image1_1 from '../../public/image/image1-1.png'
+import image1_2 from '../../public/image/image1-2.png'
+import image2 from '../../public/image/image2.png'
+import image3 from '../../public/image/image3.png'
+import image4 from '../../public/image/image4.png'
+import image5 from '../../public/image/image5.png'
+import image6 from '../../public/image/image6.png'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <h1 className="text-blue main_header">We make  creative media that <span className="text-darkblue">adds value</span></h1>
+    </div>
+    <div className="content__wrap">
+    <div className="content__container flex flex-row flex-wrap justify-between">
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <div className="content_item_image__wrap">
+          </div>
+          <Image 
+            src={image1_1}
+            alt='preview'
+            width={182}
+            placeholder="blur"
+          /> 
+          <div className="mt-6">
+          <Image 
+            src={image1_2}
+            alt='preview'
+            width={182}
+            placeholder="blur"
+          /> 
+          </div>
+          
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Photography</div>
+          <div className="font-bold mt-3 content_item_description__list">
+            <ul className="list-disc">
+              <li>Drone Photography</li>
+              <li>Interiors</li>
+              <li>Exteriors / Architectural</li>
+            </ul>
+          </div>
+          <div className="font-semibold mt-3 content_item_description__text">Every photograph we produce finds the beauty in your property while serving as a profitable asset.</div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <Image 
+            src={image2}
+            alt='preview'
+            height={333}
+            placeholder="blur"
+          /> 
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Virtual Staging</div>
+          <div className="font-semibold mt-3 content_item_description__text">
+            <p>Our staging will help you sell the potential of your space.</p>
+            <p className="mt-2">We can work off of our own photography or photos you provide.</p>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <Image 
+            src={image3}
+            alt='preview'
+            height={333}
+            placeholder="blur"
+          /> 
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Renderings</div>
+          <div className="font-bold mt-3 content_item_description__list">
+            <ul className="list-disc">
+              <li>Interiors</li>
+              <li>Exteriors</li>
+            </ul>
+          </div>
+          <div className="font-semibold mt-3 content_item_description__text">Renderings are the gold standard in pre-construction marketing.</div>
+        </div>
       </div>
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <Image 
+            src={image4}
+            alt='preview'
+            height={333}
+            placeholder="blur"
+          /> 
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Interactive Tours</div>
+          <div className="font-bold mt-3 content_item_description__list">
+            <ul className="list-disc">
+              <li>Matterport</li>
+              <li>Video Walkthroughs</li>
+            </ul>
+          </div>
+          <div className="font-semibold mt-3 content_item_description__text">Give your audience the ability to explore your property without requiring in-person travel.</div>
+        </div>
+      </div>
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <Image 
+            src={image5}
+            alt='preview'
+            height={333}
+            placeholder="blur"
+          /> 
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Floor Plans</div>
+          <div className="font-bold mt-3 content_item_description__list">
+            <ul className="list-disc">
+              <li>On-site Measure</li>
+              <li>2D Floor Plans</li>
+              <li>3D Floor Plans</li>
+            </ul>
+          </div>
+          <div className="font-semibold mt-3 content_item_description__text">We produce laser-precise floor plans faster than any other service.</div>
+        </div>
+      </div>
+      <div className="mt-10 bg-white content_item__wrap">
+        <div className="content_item_image__container">
+          <Image 
+            src={image6}
+            alt='preview'
+            height={333}
+            placeholder="blur"
+          /> 
+        </div>
+        <div className="text-darkgray content_item_description__container">
+          <div className="font-extrabold content_item_description__title">Video</div>
+          <div className="font-bold mt-3 content_item_description__list">
+            <ul className="list-disc">
+              <li>Drone Cinematography</li>
+              <li>Interiors</li>
+              <li>Exteriors / Architectural</li>
+            </ul>
+          </div>
+          <div className="font-semibold mt-3 content_item_description__text">Video content has higher engagement and will help your property stand out online.</div>
+        </div>
+      </div>
+    </div>
+    <div className="main_button_wrap">
+       <button className="main_button bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4">
+          Get STARTED
+      </button>
+    </div>
+   
+    </div>
     </main>
+    
   );
 }
